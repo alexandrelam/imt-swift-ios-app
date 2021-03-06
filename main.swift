da06e354df5c3603296be2f0b0a5d75c46fe3ca6
alexandrelam <alexandrelam@outlook.com>
2021-03-06 18:24:35 +0100

    added sum of array

--- main.swift
+++ main.swift
@@ -1,3 +1,11 @@
+func complexeSum(complexes: [Complexe]) -> Complexe{
+    var res = Complexe()
+    for complexe in complexes{
+        res = res.sum(complexe: complexe)
+    }
+    return res
+}
+
 var c1: Complexe = Complexe(reel: 2.0, imaginaire: 4.0)
 
 print(c1.description)
@@ -14,3 +22,7 @@ print(sum.description)
 
 var mul: Complexe = c1.mul(complexe: c2)
 print(mul.description)
+
+let array_complexes: [Complexe] = [c1, c2]
+let sum_complexes: Complexe = complexeSum(complexes: array_complexes)
+print(sum_complexes.description)
